d7534b810c6705fda0e7bba12ad7b0d1e2c90f48
A.R <roziboyev614@gmail.com>
2023-09-11 15:44:35 +0700

    Create lambda_map_filter.py
    I created new function

---
+++ Functions/lambda_map_filter.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 14:50:04 2023
+
+@author: Lenovo
+"""
+# import math
+# p = math.pi
+# a = 4
+# r = 3
+
+# uzunlik = lambda p, r : 2*p*r
+# print(uzunlik(math.pi,1111))
+
+# def daraja(n):
+#     return lambda x : x**n
+
+# kvadrat = daraja(2)
+# beshinchi = daraja(5)
+# print(kvadrat(2))
+# print(beshinchi(5))
+
+# from math import sqrt
+# # sonlar = list(range(1,11,2))
+# # ildizlar = list(map(sqrt,sonlar))
+# # print(ildizlar)
+# # print(sonlar)
+
+# sonlar = list(range(0,11,2))
+
+# def daraja(n):
+#     """Sonning kvadratini qaytaruvchi funksiya"""
+#     return n*n
+
+# print(list(map(daraja, sonlar)))
+
+# a = [1, 3, 4, 6]
+# b = [4, 6, 9, 10]
+
+# a_plus_b = list(map(lambda x,g : x + g, a, b))
+# print(a_plus_b)
+
+# ismlar = ['asadbek', 'ilhomboy', 'artur', 'kamronbek','vali']
+
+# print(list(map(lambda matn : matn.upper(), ismlar)))
+
+# import random 
+# sonlar =random.sample(range(100),10)
+
+# def juftmi(x):
+#     return x%2==0
+
+# juft_sonlar = list(filter(juftmi, sonlar))
+# print(sonlar)
+# print(juft_sonlar)
+
+# import random
+
+# sonlar = random.sample(range(100),10)
+
+# juft_sonlar = list(filter(lambda x: x%2==0, sonlar))
+# print(sonlar)
+# print(juft_sonlar)
+
+
+# ismlar = ['asadbek', 'ilhomboy', 'artur', 'kamronbek','vali']
+
+# ism_o = list(filter(lambda ism : ism.startswith('a'), ismlar))
+# print(ism_o)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
