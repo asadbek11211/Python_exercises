d707e708652c9bedcad0a3fe6bcd3dd270e68fac
A.R <108076725+asadbek11211@users.noreply.github.com>
2026-05-06 16:17:41 +0500

    Update soning_yuzlar_xonasini_topish.py

--- soning_yuzlar_xonasini_topish.py
+++ soning_yuzlar_xonasini_topish.py
@@ -4,6 +4,7 @@ Created on Sat Feb 18 20:54:33 2023
  masalaning qo'yilishi:
      999 dan katta bo'lgan son berilgan. Bir marta bo'lib butunini va bo'lib qoldiqini olish
      operatsiyasidan foydalanib berilgan sonni yuzlar xonasidagi sonni aniqlovchi pragramma tuzilsiz
+     
 """
 son = int(input("999 dan katta bo'lgan son kiriting : "))
 
