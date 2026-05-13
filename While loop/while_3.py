765ab38b551f199a2eca838c0048859245b0c712
A.R <108076725+asadbek11211@users.noreply.github.com>
2026-05-13 16:16:17 +0500

    Update while_3.py

--- While loop/while_3.py
+++ While loop/while_3.py
@@ -5,6 +5,7 @@ Created on Thu Jan 26 15:59:53 2023
 Kiritilgan sonning ildizini qaytaruvchi dastur.
 """
 
+
 print("Kiritilgan sonning ildizini qaytaruvchi dastur.\n")
 savol = "Musbat son kiriting "
 savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
